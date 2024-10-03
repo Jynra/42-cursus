aa89e9aee11fad12c8271db5ca26230010a7bbfc
Eliot Lucas <ellucas@c1r5s1.42lausanne.ch>
2024-10-03 15:08:33 +0200

    memset

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,11 +6,21 @@
 /*   By: ellucas <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:52:49 by ellucas           #+#    #+#             */
-/*   Updated: 2024/10/01 17:26:28 by ellucas          ###   LAUSANNE.ch       */
+/*   Updated: 2024/10/03 14:59:00 by ellucas          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_memset(void *str, int c, size_t len)
+void	*ft_memset(void *str, int c, size_t len)
 {
+	size_t				i;
+	unsigned char		*ptr;
 
+	i = 0;
+	ptr = (unsigned char *)str;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (str);
 }
