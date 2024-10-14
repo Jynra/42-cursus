efd2a225d8bb94bd4d4f17d968c4c6d2733443ec
jynra <https://jynra@github.com>
2024-10-14 10:22:55 +0200

    update

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,23 +6,25 @@
 /*   By: ellucas <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 19:00:33 by ellucas           #+#    #+#             */
-/*   Updated: 2024/10/14 09:15:24 by ellucas          ###   LAUSANNE.ch       */
+/*   Updated: 2024/10/14 10:21:41 by ellucas          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*ptr;
 
+	ptr = (unsigned char *)s;
 	i = 0;
 	if (!s)
 		return (0);
 	while (i < n)
 	{
-		if (s[i] == (unsigned char)c)
-			return ((char *)s + i);
+		if (ptr[i] == (unsigned char)c)
+			return (ptr + i);
 		i++;
 	}
 	return (0);
