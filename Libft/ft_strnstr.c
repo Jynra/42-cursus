535bb5e6d3900d06cb3492d46588a913ee4a90fa
Eliot Lucas <jynra@MacBook-Pro-de-Eliot.local>
2024-10-15 10:15:22 +0200

    update

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ellucas <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:28:35 by ellucas           #+#    #+#             */
-/*   Updated: 2024/10/14 18:07:44 by ellucas          ###   LAUSANNE.ch       */
+/*   Updated: 2024/10/15 10:11:53 by ellucas          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	n = 0;
 	i = 0;
 	j = 0;
-	if (len < 0 && haystack[i] == '\0')
+	if (len < 0 || (haystack[i] == '\0' && needle[i] != '\0'))
 		return (0);
 	if (needle[i] == '\0')
 		return ((char *)haystack);
@@ -40,3 +40,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (0);
 }
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int ac, char *av[])
+{
+	printf("%s\n", ft_strnstr(av[1], av[2], atoi(av[3])));
+}
+*/
