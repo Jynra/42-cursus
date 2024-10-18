bcc75f45aa16aea8d464d15c2102c1c1db5cd47f
Eliot Lucas <ellucas@c1r5s2.42lausanne.ch>
2024-10-18 15:57:34 +0200

    update

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,42 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ellucas <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/18 12:38:58 by ellucas           #+#    #+#             */
-/*   Updated: 2024/10/18 15:15:05 by ellucas          ###   LAUSANNE.ch       */
+/*   Created: 2024/10/18 15:16:06 by ellucas           #+#    #+#             */
+/*   Updated: 2024/10/18 15:56:37 by ellucas          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char *ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-	size_t	size;
 	size_t	i;
-	size_t	j;
+	char	*str;
 
-	if (s1 && s2)
-	{
-		size = ft_strlen(s1) + ft_strlen(s2);
-		str = malloc(size + 1);
-		if (str == NULL)
-			return (0);
-		i = 0;
-		while (i < size)
-		{
-			j = 0;
-			while (s1[j] != '\0')
-				str[i++] = s1[j++];
-			j = 0;
-			while (s2[j] != '\0')
-				str[i++] = s2[j++];
-		}
-		str[i] = '\0';
-		return (str);
-	}
-	return (0);
+	str = malloc()
 }
