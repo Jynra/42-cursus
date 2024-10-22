10113637773cc1788f84befa795e5f37d4f592bb
jynra <https://jynra@github.com>
2024-10-22 02:00:52 +0200

    update

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,16 +6,31 @@
 /*   By: ellucas <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 15:16:06 by ellucas           #+#    #+#             */
-/*   Updated: 2024/10/18 15:56:37 by ellucas          ###   LAUSANNE.ch       */
+/*   Updated: 2024/10/22 01:59:34 by ellucas          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
+	int		start;
+	int		end;
 	char	*str;
 
-	str = malloc()
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	str = malloc(end - start + 2);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, &s1[start], end - start + 2);
+	return (str);
 }
